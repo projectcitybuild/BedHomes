6a76a977e13ebe0ca5b5419bda8186dff5816043
Jabelpeeps <jabelpeeps@gmail.com>
2016-08-12 21:48:21 +0100

    adding /spawn command and some more config options

--- src/org/jabelpeeps/bedhomes/BedHomes.java
+++ src/org/jabelpeeps/bedhomes/BedHomes.java
@@ -16,10 +16,9 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -49,8 +48,14 @@ public final class BedHomes extends JavaPlugin implements Listener {
 
     protected String logonMsg;
     private String brokenMsg;   
-    private int warmupDelay;
-    protected static int cost;
+    private int homeWarmupDelay;
+    protected int homeCost;
+    private boolean homeEnabled;
+    
+    private int spawnWarmupDelay;
+    protected int spawnCost;
+    private boolean spawnEnabled;
+    private String spawnWorld;
     
     protected File bedsYml;
     protected FileConfiguration saveData;
@@ -76,14 +81,23 @@ public final class BedHomes extends JavaPlugin implements Listener {
     public void onEnable() {
         saveDefaultConfig();
         FileConfiguration config = getConfig();
-        warmupDelay = config.getInt( "warmup" );
-        cost = config.getInt( "cost" );
+        debug = config.getBoolean( "debug" );
         
-        bedsYml = new File(getDataFolder(), "beds.yml");
+        ConfigurationSection homeSection = config.getConfigurationSection( "home" );
+        homeWarmupDelay = homeSection.getInt( "warmup" );
+        homeCost = homeSection.getInt( "cost" );
+        homeEnabled = homeSection.getBoolean( "enabled" );
+        
+        ConfigurationSection spawnSection = config.getConfigurationSection( "spawn" );
+        spawnWarmupDelay = spawnSection.getInt( "warmup" );
+        spawnCost = spawnSection.getInt( "cost" );
+        spawnEnabled = spawnSection.getBoolean( "enabled" );
+        spawnWorld = spawnSection.getString( "world" );
+        
+        bedsYml = new File( getDataFolder(), "beds.yml" );
         saveData = YamlConfiguration.loadConfiguration( bedsYml );
         
         Bukkit.getPluginManager().registerEvents( this, this );
-        ConfigurationSerialization.registerClass( Location.class );
        
         // load the saved bed locations.
         for ( String each : saveData.getKeys( false ) ) {
@@ -94,9 +108,17 @@ public final class BedHomes extends JavaPlugin implements Listener {
         }
         if ( !setupEconomy() ) 
             logger.log( Level.WARNING, "Vault +/- Economy plugins not found, cost per command will not function." );
-        
+               
         // saves the bed locations every 10 mins.
         saveBeds.runTaskTimerAsynchronously( this, 12000, 12000 );
+        
+        // check that the configured world for the spawn command is valid.
+        if ( spawnEnabled && Bukkit.getWorld( spawnWorld ) == null 
+                                && !spawnWorld.equalsIgnoreCase( "current" ) ) {
+            
+            logger.log( Level.WARNING, "The configured spawn world '" + spawnWorld + "' was not found. /spawn disabled." );
+            spawnEnabled = false;          
+        }          
     }
     
     private boolean setupEconomy() {
@@ -110,9 +132,9 @@ public final class BedHomes extends JavaPlugin implements Listener {
     }
     
     @Override
-    public void onDisable() {  
-       
+    public void onDisable() {         
         Bukkit.getScheduler().cancelTasks( this );
+        
         // one last run of the save routine before shutdown.
         saveBeds.run();          
     }
@@ -122,7 +144,7 @@ public final class BedHomes extends JavaPlugin implements Listener {
         // sends a message to players telling them to set their beds in the new plugin.
         
         Player player = event.getPlayer();
-        
+                
         if ( logonMsg == null )
             logonMsg = String.join( "", 
                     ChatColor.RED.toString(), ChatColor.BOLD.toString(), "IMPORTANT: ", System.lineSeparator(),
@@ -132,13 +154,8 @@ public final class BedHomes extends JavaPlugin implements Listener {
         
         if ( !beds.containsKey( player.getUniqueId() ) ) {
             
-            new BukkitRunnable() {                   
-                @Override
-                public void run() {
-                    player.sendMessage( logonMsg );
-                }        
-            }.runTaskLater( this, 100 );           
-        }    
+            Bukkit.getScheduler().runTaskLater( this, () -> player.sendMessage( logonMsg ), 100 );   
+        }
     }
     
     
@@ -146,7 +163,7 @@ public final class BedHomes extends JavaPlugin implements Listener {
     public void onUsingBed( PlayerBedEnterEvent event ) {
         // records the location where the player was standing when they clicked the bed
         // (which is assumed to be safe to return them to).
-        // sets that location as the API BedSpawnLocation, and also saves it to our own hashmap.
+        // Sets that location as the API BedSpawnLocation, and also saves it to our own hashmap.
         
         Player player = event.getPlayer();
             
@@ -201,13 +218,16 @@ public final class BedHomes extends JavaPlugin implements Listener {
     @Override
     public boolean onCommand( CommandSender sender, Command cmd, String label, String[] args ) {
         
-        if ( sender instanceof ConsoleCommandSender ) {
+        if ( !(sender instanceof Player) ) {
             sender.sendMessage( "This command is only available to players" );
+            return true;
         }
         
-        if ( sender instanceof Player && cmd.getName().equalsIgnoreCase( "home" ) ) { 
+        Player player = (Player) sender;
+        String command = cmd.getName();
+        
+        if ( homeEnabled && command.equalsIgnoreCase( "home" ) ) { 
             
-            Player player = (Player) sender;
             Location myBed = getBedFor( player );
             
             if ( myBed == null ) {
@@ -215,20 +235,38 @@ public final class BedHomes extends JavaPlugin implements Listener {
                         ChatColor.YELLOW.toString().concat( "No saved bed location for you.  Did you sleep in one yet?" ) );
                 return true;
             }
-            if ( economy != null && !economy.has( player, cost ) ) {
-                player.sendMessage( String.join( "", 
-                        ChatColor.YELLOW.toString(), "using /home costs ", 
-                        String.valueOf( cost ), economy.currencyNamePlural(), " each time you use it." ) );
-                return true;
-            }
-            warmups.put( player.getUniqueId(), 
-                         new DelayedTeleport( player, myBed ).runTaskLater( this, 20 * warmupDelay ) );
+            checkEconAndTeleport( player, myBed, command, homeCost, homeWarmupDelay );  
             
-            player.sendMessage( String.join( "", 
-                    ChatColor.RED.toString(), "Do Not Move for ", 
-                    String.valueOf( warmupDelay ), " seconds to teleport home." ) );         
-        } 
+        }         
+        else if ( spawnEnabled && command.equalsIgnoreCase( "spawn" ) ) {
+            
+            Location spawn = null;
+            
+            if ( spawnWorld.equalsIgnoreCase( "current" ) ) 
+                spawn = player.getWorld().getSpawnLocation();
+            else 
+                spawn = Bukkit.getWorld( spawnWorld ).getSpawnLocation();
+                       
+            checkEconAndTeleport( player, spawn, command, spawnCost, spawnWarmupDelay );            
+        }
         return true; 
+    }
+    
+    private void checkEconAndTeleport( Player player, Location loc, String cmd, int cost, int delay ) {
+        
+        if ( economy != null ) {
+            player.sendMessage( String.join( "",  ChatColor.YELLOW.toString(), "using /", cmd, " costs ", 
+                    String.valueOf( cost ), economy.currencyNamePlural(), " each time you use it." ) );
+            
+            if ( !economy.has( player, cost ) )
+                return;
+        }
+        
+        warmups.put( player.getUniqueId(), 
+                     new DelayedTeleport( player, loc, cost ).runTaskLater( this, 20 * delay ) );
+        
+        player.sendMessage( String.join( "", ChatColor.RED.toString(), "Do Not Move for ", 
+                            String.valueOf( delay ), " seconds to teleport." ) );         
     }
     
     @EventHandler
@@ -238,9 +276,8 @@ public final class BedHomes extends JavaPlugin implements Listener {
         
         if ( warmups.containsKey( uuid ) ) {
             warmups.remove( uuid ).cancel();
-            event.getPlayer().sendMessage( String.join( "", 
-                    ChatColor.RED.toString(), "Movement Detected!", System.lineSeparator(),
-                    ChatColor.RESET.toString(), "/home command cancelled." ) );
+            event.getPlayer().sendMessage( String.join( "",  ChatColor.RED.toString(), "Movement Detected!", 
+                    System.lineSeparator(), ChatColor.RESET.toString(), "Teleport cancelled." ) );
         } 
     }
     
@@ -259,26 +296,25 @@ public final class BedHomes extends JavaPlugin implements Listener {
         
         private final Player p;
         private final Location loc;
+        private final int cost;
         
-        DelayedTeleport( Player player, Location location ) {
+        DelayedTeleport( Player player, Location location, int _cost ) {
             p = player;
             loc = location;
+            cost = _cost;
         }
         
         @Override
         public void run() {
             warmups.remove( p.getUniqueId() );
             
-            if ( economy == null || economy.withdrawPlayer( p, cost ).transactionSuccess() ) {
+            if ( economy == null || economy.withdrawPlayer( p, homeCost ).transactionSuccess() ) {
                 p.teleport( loc, TeleportCause.COMMAND );
-                p.sendMessage( String.join( "", 
-                        ChatColor.YELLOW.toString(), "You have been charged ", 
-                        String.valueOf( cost ), economy.currencyNamePlural(), " for using /home." ) );
-            }
-            else
-                p.sendMessage( String.join( "", 
-                        ChatColor.YELLOW.toString(), "using /home costs ", 
+                
+                if ( economy != null )
+                    p.sendMessage( String.join( "", ChatColor.YELLOW.toString(), "You have been charged ", 
                         String.valueOf( cost ), economy.currencyNamePlural() ) );
+            }
         }      
     }
 }
